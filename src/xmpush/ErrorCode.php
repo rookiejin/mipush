b9c985299cbab331b6fa10df2bfdfcd42073d2e1
Mj <Mj@mj.com>
2018-01-22 16:19:35 +0800

    init

---
+++ src/xmpush/ErrorCode.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 错误码定义.
+ * @author wangkuiwei
+ * @name ErrorCode
+ * @desc 错误码
+ *
+ */
+namespace Rookiejin\Xmpush;
+
+class ErrorCode {
+    const Success = 0;
+    const NETWORK_ERROR_TIMEOUT = -1;
+}
---
+++ src/xmpush/Result.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 服务器返回的结果.
+ * @author wangkuiwei
+ * @name Result
+ * @desc Sender发送消息后，服务器返回的结果。
+ *
+ */
+namespace Rookiejin\Xmpush;
+
+class Result {
+    private $errorCode;
+    private $raw;
+
+    public function __construct($jsonStr) {
+        $data = json_decode($jsonStr, true);
+        $this->raw = $data;
+        $this->errorCode = $data['code'];
+    }
+
+    public function getErrorCode() {
+        return $this->errorCode;
+    }
+
+    public function getRaw() {
+        return $this->raw;
+    }
+}
